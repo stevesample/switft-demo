9987028bd41e74d44ede10fd2a91583b9f2a15b3
Steve Sample <steve.sample22@gmail.com>
2014-06-07 15:10:21 -0700

    Initial Commit

---
+++ Swift_Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_Demo
+//
+//  Created by Steve Sample on 6/7/14.
+//  Copyright (c) 2014 Steve Sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
